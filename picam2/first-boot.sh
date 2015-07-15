4fdbe517f326fb84930d6737ba51daf6d97a5a4f
Kenny <pi@picam2.(none)>
2015-07-15 13:23:10 +0000

    First pass at picam2

---
+++ picam2/first-boot.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Must run as root to install packages"
+    exec sudo $HOME/camera-automation/$(hostname)/first-boot.sh
+fi
+
+echo "############################################################"
+echo "# Configuring this pi as $(hostname)"
+echo "# Please set a password and then enable the camera"
+echo "############################################################"
+
+passwd pi
+raspi-config
+update-rc.d geiger defaults
+apt-get update
+xargs -ra /home/pi/camera-automation/$(hostname)/package-dependencies.list apt-get install
+rm /home/pi/.bash_login
+raspi-config --expand-rootfs
---
+++ picam2/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Using root directory: ${ROOT:=${1:-.}}"
+
+install -o 1000 -g 1000 -m 755 -d ${ROOT}/home/pi/video/
+install -o 1000 -g 1000 -m 700 -d ${ROOT}/home/pi/.ssh/
+ln -sf ../camera-automation/picam2/authorized_keys2 ${ROOT}/home/pi/.ssh/
+ln -sf camera-automation/picam2/go_cameras_go.sh ${ROOT}/home/pi/
+ln -sf camera-automation/picam2/CameraScripts ${ROOT}/home/pi/
+ln -sf camera-automation/picam2/first-boot.sh ${ROOT}/home/pi/.bash_login
+ln -sf camera-automation/picam2/gpio.sh ${ROOT}/home/pi/
+sed -i '/^# Print the IP address/i/home/pi/gpio.sh\n' ${ROOT}/etc/rc.local
+ln -sf ../../home/pi/camera-automation/picam2/interfaces ${ROOT}/etc/network/
+ln -sf ../home/pi/camera-automation/picam2/hosts ${ROOT}/etc/
+ln -sf ../home/pi/camera-automation/picam2/hostname ${ROOT}/etc/
+ln -sf ../home/pi/camera-automation/picam2/resolvconf.conf ${ROOT}/etc/
+
+cd $ROOT
+git clone https://github.com/psas/GeigerLog.git
+ln -sf ../home/pi/GeigerLog/init.d-geiger ${ROOT}/etc/init.d/geiger
+
+echo "Done!"
