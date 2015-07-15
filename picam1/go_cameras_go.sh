1c62491ed91b9d332e2529e7d7a7225c8b6564a5
Kenny <pi@picam1.(none)>
2015-07-15 08:55:01 +0000

    Stream video to fc and trust it to do the right thing

--- picam1/go_cameras_go.sh
+++ picam1/go_cameras_go.sh
@@ -6,7 +6,7 @@ raspivid -n -w 640 -h 480 -b 500000 -fps 30 -rot 270 -t 0 -o - | \
     ! h264parse \
     ! queue \
     ! rtph264pay config-interval=10 pt=96 \
-    ! udpsink host=10.0.3.1 port=30000 \
+    ! udpsink host=10.10.10.10 port=30000 \
     splitter. \
     ! queue \
     ! filesink location="videofile.h264"
