dd4b64d784ab790455e3f61909dd7bb3c278649e
Philip Arola <philiparola@hotmail.com>
2015-07-15 12:54:24 +0000

    Bump up the resolution

--- picam2/go_cameras_go.sh
+++ picam2/go_cameras_go.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-raspivid -n -w 1280 -h 720 -b 500000 -fps 30 -rot 270 -t 0 \
+raspivid -n -w 1920 -h 1080 -b 500000 -fps 30 -rot 270 -t 0 \
     -o $(mktemp /home/pi/video/XXXXXX.h264) &
