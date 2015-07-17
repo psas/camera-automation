c6f2b884db66186bd231ca0e728f0996aca27012
Kenny <kenny@romhat.net>
2015-07-17 02:29:24 -0700

    cd - in case of relative ROOT

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ export ROOT=${2:-.}
 cd ${ROOT}/home/pi/
 git clone https://github.com/psas/camera-automation.git
 chown -R 1000:1000 camera-automation
+cd -
 exec ${ROOT}/home/pi/camera-automation/${HOST}/install.sh
